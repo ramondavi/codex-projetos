9ada1189655663d4f78e79c65295d9b44a4674f9
Ramon Santana <ramondavi@outlook.com>
2026-09-21 20:50:12 -0300

    feat: adiciona barra do Governo Brasileiro

---
+++ src/components/government-bar.tsx
@@ -0,0 +1,3 @@
+export function GovernmentBar() {
+  return <div className="government-bar"><a href="https://brasil.gov.br/" target="_blank" rel="noreferrer">Portal do Governo Brasileiro</a></div>;
+}
